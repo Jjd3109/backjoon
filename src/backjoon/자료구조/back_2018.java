c89fe026548ba48bb6ca08161cfacae9565d2bf1
jjd3109 <jongdoo2001@naver.com>
2024-12-13 23:25:03 +0900

    백준 / 2018

---
+++ src/backjoon/자료구조/back_2018.java
@@ -0,0 +1,37 @@
+package backjoon.자료구조;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class back_2018 {
+
+    public static void main(String[] args) throws IOException {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+
+        int count = 1;
+        int start_index = 1;
+        int end_index = 1;
+        int sum = 1;
+
+        while(end_index != N){
+            if(sum == N){ // 현재 연속 합이 N과 같은 경우
+                count ++;
+                end_index++;
+                sum = sum + end_index;
+            } else if (sum > N){
+                sum = sum - start_index;
+                start_index++;
+            } else {
+                end_index+++;
+                sum = sum + end_index;
+            }
+        }
+
+        System.out.println(count);
+
+    }
+}
